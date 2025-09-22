20db8bb89dcf434655ab0fcc55320cd07cfaefb8
Hernan Keena <hpkeena@inti.gob.ar>
2025-09-21 23:45:44 -0300

    fix

--- src/incializar.db.desde.ics.ts
+++ src/incializar.db.desde.ics.ts
@@ -4,15 +4,25 @@ import { EventoInterpretado, interpretar } from "./calendario/interprete.eventos
 import { JSONDB } from './db/json.db';
 
 const jsonDB = new JSONDB<EventoInterpretado>(gestorArchivosLocal);
-jsonDB.cargar("./data/db.json");
+const dbFile = "./data/db.json";
+jsonDB.cargar(dbFile);
 
 const currentYear = new Date().getFullYear();
 const minTime = new Date(`${currentYear}-01-01T00:00:00Z`);
 const maxTime = new Date(`${currentYear}-12-31T23:59:59Z`);
 
 crearGestorCalendarioLocal("./data/data.ics")
-  .then(async g => {
-    await g.buscar(minTime, maxTime )
-    .map(e => interpretar(e))
-    .forEach(e => jsonDB.agregarRegistro(e));
+  .then(g => {
+    console.log("Gestor calendario local creado buscando eventos");
+    return g.buscar(minTime, maxTime);
+  })
+  .then(eventos => {
+    console.log("Eventos encontrados: " + eventos.length);
+    eventos.map(e => interpretar(e))
+    .forEach(e => {
+      console.log("Agregando => " + JSON.stringify(e));
+      jsonDB.agregarRegistro(e)}
+    );
+    jsonDB.guardar(dbFile);
+    console.log("Fin");
   });
